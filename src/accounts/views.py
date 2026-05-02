3c6f7f5a0f7bbd19de3c13b00af5df126501cf60
Kübra Alkayış <kubraalkayis@Kubra-MacBook-Air-2.local>
2026-05-02 17:09:58 +0300

    accounts kayit formu ve register sistemi eklendi

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
 
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("register")
+    else:
+        form = RegisterForm()
+
+    return render(request, "accounts/register.html", {"form": form})
 # Create your views here.
