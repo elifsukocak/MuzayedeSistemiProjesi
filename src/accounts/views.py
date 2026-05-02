afdfc8802e69c191fc6f5b40a16df8a3266a7d5f
Kübra Alkayış <kubraalkayis@Kubra-MacBook-Air-2.local>
2026-05-02 17:50:38 +0300

    profile sayfasi login gerektirir hale getirildi

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm
 
 def register_view(request):
@@ -36,5 +37,6 @@ def logout_view(request):
     logout(request)
     return redirect("login")
 
+@login_required
 def profile_view(request):
     return render(request, "accounts/profile.html")
