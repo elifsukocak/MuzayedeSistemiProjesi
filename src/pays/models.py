69095fc25d732c35e39418a75d54e4ee774b2175
sena <84151030+padfootss@users.noreply.github.com>
2026-05-11 23:55:15 +0300

    Wallet modeline guncellenme_tarihi alani eklendi

--- src/pays/models.py
+++ src/pays/models.py
@@ -12,9 +12,10 @@ class Wallet(models.Model):
         related_name='wallet',
         verbose_name='Kullanici',
     )
-     bakiye = models.DecimalField(
+    bakiye = models.DecimalField(
         max_digits=12,
         decimal_places=2,
         default=Decimal('10000.00'),
         verbose_name='Sahte Para Bakiyesi',
-    )
+     )
+     guncellenme_tarihi = models.DateTimeField(auto_now=True)
