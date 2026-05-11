07d564ec17115bfb2b54c8bf3d7a5ac3eafe8d72
sena <84151030+padfootss@users.noreply.github.com>
2026-05-12 00:14:53 +0300

    add_demo_money fonksiyonu olusturuldu

--- src/pays/services.py
+++ src/pays/services.py
@@ -9,3 +9,12 @@ def get_wallet(user):
     wallet, _ = Wallet.objects.get_or_create(user=user)
     return wallet
 
+
+    @transaction.atomic
+def add_demo_money(user, amount):
+    wallet = Wallet.objects.select_for_update().get_or_create(user=user)[0]
+    wallet.bakiye += Decimal(str(amount))
+    wallet.full_clean()
+    wallet.save(update_fields=['bakiye', 'guncellenme_tarihi'])
+    return wallet
+
