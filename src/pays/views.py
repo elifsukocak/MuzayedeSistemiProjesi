0c603bba16b096b6361bef48c601e662bf2a101f
sena <84151030+padfootss@users.noreply.github.com>
2026-05-12 00:40:40 +0300

    odeme_paneli wallet gosterimi eklendi

--- src/pays/views.py
+++ src/pays/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from decimal import Decimal, InvalidOperation
 
-# Create your views here.
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+
+from .services import add_demo_money, get_wallet
+
+
+@login_required
+def odeme_paneli(request):
+    wallet = get_wallet(request.user)
