3984694a56b927d943dcc168b2db4b7ed0e0e652
sena <84151030+padfootss@users.noreply.github.com>
2026-05-11 23:51:48 +0300

    Wallet modeli olusturuldu

--- src/pays/models.py
+++ src/pays/models.py
@@ -1,3 +1,14 @@
+from decimal import Decimal
+
+from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+
+class Wallet(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='wallet',
+        verbose_name='Kullanici',
+    )
