4ec763f7bfd304d93caeaa2e50e51981c2c1be81
Zeynep <zeynepk062006@gmail.com>
2026-05-12 18:53:49 +0300

    Teklif verme kurallarini test eder

--- src/bids/views.py
+++ src/bids/views.py
@@ -23,3 +23,32 @@ def teklif_ver(request, auction_id):
         messages.error(request, mesaj)
 
     return redirect('auction_detail', id=auction.id)
+
+@login_required
+def artis_adimi_guncelle(request, auction_id):
+    auction = get_object_or_404(Auction, pk=auction_id)
+
+    if auction.product.satici_id != request.user.id:
+        messages.error(request, "Bu muzayedenin artis adimini sadece urunun saticisi guncelleyebilir.")
+        return redirect('auction_detail', id=auction.id)
+
+    teklif_ayari, _ = BidIncrement.objects.get_or_create(auction=auction)
+
+    if request.method == 'POST':
+        try:
+            artis_adimi = Decimal(str(request.POST.get('artis_adimi')))
+        except (InvalidOperation, TypeError):
+            messages.error(request, "Gecerli bir artis adimi giriniz.")
+            return redirect('bids:artis_adimi_guncelle', auction_id=auction.id)
+
+        teklif_ayari.artis_adimi = artis_adimi
+        try:
+            teklif_ayari.full_clean()
+        except Exception as exc:
+            messages.error(request, exc)
+        else:
+            teklif_ayari.save()
+            messages.success(request, "Artis adimi guncellendi.")
+            return redirect('auction_detail', id=auction.id)
+
+    return render(request, 'bids/artis_adimi_form.html', {'auction': auction, 'teklif_ayari': teklif_ayari})
