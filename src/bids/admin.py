c0043c94ee19e08a3b9c7b96bedeb67dbe5dabf1
Zeynep <zeynepk062006@gmail.com>
2026-05-12 17:45:27 +0300

    Bid modeli için admin paneli yapılandırıldı

--- src/bids/admin.py
+++ src/bids/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import Bid
+
+
+@admin.register(Bid)
+# Bid modelini admin paneline kaydet
+class BidAdmin(admin.ModelAdmin):
+    list_display = ('auction', 'kullanici', 'miktar', 'durum', 'zaman')
+    list_filter = ('durum', 'zaman')
+    search_fields = ('auction__product__ad', 'kullanici__username')
+    readonly_fields = ('zaman',)
