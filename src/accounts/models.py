b7d907f4107309cccd9a447bf86c09fcab146393
Kübra Alkayış <kubraalkayis@Kubra-MacBook-Air-2.local>
2026-05-13 18:57:29 +0300

    Hesap silme koduna sure kontrolu eklendi

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from datetime import timedelta
+
+class AccountDeleteCode(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    code = models.CharField(max_length=4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def is_expired(self):
+        return timezone.now() > self.created_at + timedelta(minutes=5)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.code}"
 
 class User(AbstractUser):
     ROLE_CHOICES = [
